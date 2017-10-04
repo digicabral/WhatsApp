b0bfa254c1af686abff17c8460f14e12c752b652
RSCabral <digicabral@gmail.com>
2017-10-04 15:54:23 -0300

    Initial commit

---
+++ app/src/main/java/cabral/com/br/whatsapp/activity/MainActivity.java
@@ -0,0 +1,23 @@
+package cabral.com.br.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import cabral.com.br.whatsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        reference.child("pontos").setValue(100);
+
+    }
+}
