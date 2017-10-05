f2d3aff86be91fb37c38fb2d11f1f426b11e3252
RSCabral <digicabral@gmail.com>
2017-10-05 15:59:12 -0300

    Mudança na forma de login

--- app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
@@ -26,112 +26,19 @@ import cabral.com.br.whatsapp.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText nome;
-    private EditText codPais;
-    private EditText codArea;
-    private EditText telefone;
-    private Button cadastrar;
-    private String[] permissions = new String[]{
-            Manifest.permission.SEND_SMS,
-            Manifest.permission.INTERNET
-            };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Permissao.validaPermissoes(1, this, permissions);
-
-        nome     = (EditText)findViewById(R.id.edit_nome);
-        codPais  = (EditText)findViewById(R.id.edit_cod_pais);
-        codArea  = (EditText)findViewById(R.id.edit_cod_area);
-        telefone = (EditText)findViewById(R.id.edit_telefone);
-        cadastrar = (Button)findViewById(R.id.btCadastrar);
-
-        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
-        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
-        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
-
-        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
-        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
-        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
-
-        codPais.addTextChangedListener(maskCodPais);
-        codArea.addTextChangedListener(maskCodArea);
-        telefone.addTextChangedListener(maskTelefone);
-
-        cadastrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nomeUsuario = nome.getText().toString();
-                String telefoneCompleto =
-                        codPais.getText().toString()+
-                        codArea.getText().toString()+
-                       telefone.getText().toString();
-                String telefoneSemFormatacao = telefoneCompleto.replace("+","");
-                telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
-
-                Random randomico = new Random();
-                int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
-                String token = String.valueOf(numeroRandomico);
-                String mensagemEnvio = "Código de confirmação do WhatsApp: "+token;
-
-                Preferencias preferencias = new Preferencias(LoginActivity.this);
-                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
-
-                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatacao, mensagemEnvio);
-
-                if(enviadoSMS){
-                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-                else
-                {
-                    Toast.makeText(LoginActivity.this, "Problema ao enviar o SMS, tente novamente!", Toast.LENGTH_LONG).show();
-                }
-                /*HashMap<String, String> usuario = preferencias.getDadosUsuario();
-                * */
-
-            }
-        });
 
     }
-    private boolean enviaSMS(String telefone, String mensagem){
-        try{
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
-            return true;
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
+
+    public void abrirCadastroUsuario(View view){
+        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+        startActivity(intent);
+        finish();
     }
 
-    public void  onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for(int resultado: grantResults){
-            if(resultado == PackageManager.PERMISSION_DENIED){
-                alertaValidacaoPermissao();
-            }
-        }
-    }
-
-    private void alertaValidacaoPermissao(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Permissões negadas");
-        builder.setMessage("Para utilizar esse app, é necessário aceitar as permissões");
-
-        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
-    }
 }
 
