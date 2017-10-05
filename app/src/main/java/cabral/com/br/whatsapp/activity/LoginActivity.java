b4b2c3272d2ea108bc35a39ebaed312971555079
RSCabral <digicabral@gmail.com>
2017-10-05 14:43:35 -0300

    Solicitando permissões

--- app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
@@ -1,11 +1,18 @@
 package cabral.com.br.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
@@ -14,6 +21,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 import cabral.com.br.whatsapp.R;
+import cabral.com.br.whatsapp.helper.Permissao;
 import cabral.com.br.whatsapp.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,11 +31,17 @@ public class LoginActivity extends AppCompatActivity {
     private EditText codArea;
     private EditText telefone;
     private Button cadastrar;
+    private String[] permissions = new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.INTERNET
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Permissao.validaPermissoes(1, this, permissions);
 
         nome     = (EditText)findViewById(R.id.edit_nome);
         codPais  = (EditText)findViewById(R.id.edit_cod_pais);
@@ -61,14 +75,63 @@ public class LoginActivity extends AppCompatActivity {
                 Random randomico = new Random();
                 int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
                 String token = String.valueOf(numeroRandomico);
+                String mensagemEnvio = "Código de confirmação do WhatsApp: "+token;
 
                 Preferencias preferencias = new Preferencias(LoginActivity.this);
                 preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
 
-                HashMap<String, String> usuario = preferencias.getDadosUsuario();
+                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatacao, mensagemEnvio);
+
+                if(enviadoSMS){
+                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Problema ao enviar o SMS, tente novamente!", Toast.LENGTH_LONG).show();
+                }
+                /*HashMap<String, String> usuario = preferencias.getDadosUsuario();
+                * */
 
             }
         });
 
     }
+    private boolean enviaSMS(String telefone, String mensagem){
+        try{
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void  onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int resultado: grantResults){
+            if(resultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar esse app, é necessário aceitar as permissões");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
+
