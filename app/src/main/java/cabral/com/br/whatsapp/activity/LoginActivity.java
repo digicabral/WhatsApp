e87e8c75527e04893b30702bff99033554956f85
RSCabral <digicabral@gmail.com>
2017-10-05 09:28:01 -0300

    Mascara nos campos de validação e token sendo gerado.

--- app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
@@ -2,14 +2,66 @@ package cabral.com.br.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import java.util.Random;
 
 import cabral.com.br.whatsapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText nome;
+    private EditText codPais;
+    private EditText codArea;
+    private EditText telefone;
+    private Button cadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        nome     = (EditText)findViewById(R.id.edit_nome);
+        codPais  = (EditText)findViewById(R.id.edit_cod_pais);
+        codArea  = (EditText)findViewById(R.id.edit_cod_area);
+        telefone = (EditText)findViewById(R.id.edit_telefone);
+        cadastrar = (Button)findViewById(R.id.btCadastrar);
+
+        SimpleMaskFormatter simpleMaskCodPais = new SimpleMaskFormatter("+NN");
+        SimpleMaskFormatter simpleMaskCodArea = new SimpleMaskFormatter("NN");
+        SimpleMaskFormatter simpleMaskTelefone = new SimpleMaskFormatter("NNNNN-NNNN");
+
+        MaskTextWatcher maskCodPais = new MaskTextWatcher(codPais, simpleMaskCodPais);
+        MaskTextWatcher maskCodArea = new MaskTextWatcher(codArea, simpleMaskCodArea);
+        MaskTextWatcher maskTelefone = new MaskTextWatcher(telefone, simpleMaskTelefone);
+
+        codPais.addTextChangedListener(maskCodPais);
+        codArea.addTextChangedListener(maskCodArea);
+        telefone.addTextChangedListener(maskTelefone);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto =
+                        codPais.getText().toString()+
+                        codArea.getText().toString()+
+                       telefone.getText().toString();
+                String telefoneSemFormatacao = telefoneCompleto.replace("+","");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
+
+                Random randomico = new Random();
+                int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
+                String token = String.valueOf(numeroRandomico);
+
+            }
+        });
+
     }
 }
