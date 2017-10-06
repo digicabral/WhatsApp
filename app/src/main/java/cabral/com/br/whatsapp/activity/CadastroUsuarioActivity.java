f13206600bb6483fbd5610e8cbcfbc0079969e55
RSCabral <digicabral@gmail.com>
2017-10-06 13:14:58 -0300

    Salvando dados de usuário

--- app/src/main/java/cabral/com/br/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/CadastroUsuarioActivity.java
@@ -12,6 +12,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import cabral.com.br.whatsapp.R;
 import cabral.com.br.whatsapp.config.ConfiguracaoFirebase;
@@ -59,6 +60,9 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     Toast.makeText(CadastroUsuarioActivity.this, "Sucesso ao cadastrar usuário", Toast.LENGTH_LONG).show();
+                    FirebaseUser userFirebase = task.getResult().getUser();
+                    usuario.setId(userFirebase.getUid());
+                    usuario.salvar();
                 }
                 else{
                     Toast.makeText(CadastroUsuarioActivity.this, "Erro ao cadastrar usuário", Toast.LENGTH_LONG).show();
--- app/src/main/java/cabral/com/br/whatsapp/model/Usuario.java
+++ app/src/main/java/cabral/com/br/whatsapp/model/Usuario.java
@@ -1,6 +1,11 @@
 package cabral.com.br.whatsapp.model;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
+import cabral.com.br.whatsapp.config.ConfiguracaoFirebase;
+
 public class Usuario {
 
     private String id;
@@ -11,6 +16,13 @@ public class Usuario {
     public Usuario(){
 
     }
+
+    public void salvar(){
+        DatabaseReference reference = ConfiguracaoFirebase.getFirebase();
+        reference.child("usuarios").child(getId()).setValue(this);
+    }
+
+    @Exclude
     public String getId() {
         return id;
     }
@@ -35,6 +47,7 @@ public class Usuario {
         this.email = email;
     }
 
+    @Exclude
     public String getSenha() {
         return senha;
     }
