50a82259a35f812c4234d4f517ae4c2e02d02a3b
RSCabral <digicabral@gmail.com>
2017-10-09 15:04:10 -0300

    Mudancas no login Criacao de toolbar Logout sendo feito pelo toolbar Icones adicionados

--- app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,10 @@ import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 
 import java.util.HashMap;
@@ -26,19 +31,72 @@ import cabral.com.br.whatsapp.R;
 import cabral.com.br.whatsapp.config.ConfiguracaoFirebase;
 import cabral.com.br.whatsapp.helper.Permissao;
 import cabral.com.br.whatsapp.helper.Preferencias;
+import cabral.com.br.whatsapp.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DatabaseReference referenciaFirebase;
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        referenciaFirebase = ConfiguracaoFirebase.getFirebase();
-        referenciaFirebase.child("pontos").setValue("800");
+        verificarUsuarioLogado();
 
+        email = (EditText)findViewById(R.id.et_login_email);
+        senha = (EditText)findViewById(R.id.et_login_senha);
+        botaoLogar = (Button)findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                validarLogin();
+            }
+        });
+
+    }
+
+    private void verificarUsuarioLogado() {
+
+        firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
+        if(firebaseAuth.getCurrentUser()!= null)
+        {
+            abrirTelaPrincipal();
+        }
+
+    }
+
+    private void validarLogin(){
+        firebaseAuth = ConfiguracaoFirebase.getFirebaseAuth();
+        firebaseAuth.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Sucesso ao logar", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Erro ao logar", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
 
     }
 
--- app/src/main/java/cabral/com/br/whatsapp/activity/MainActivity.java
+++ app/src/main/java/cabral/com/br/whatsapp/activity/MainActivity.java
@@ -1,8 +1,16 @@
 package cabral.com.br.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,11 +19,48 @@ import cabral.com.br.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_adicionar:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario() {
+        autenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
